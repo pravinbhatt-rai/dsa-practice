31eb12c1d0e667b34b2e0732740730567f28804e
Pravin Bhattarai <pravinbhattarai45@gmail.com>
2026-08-10 13:17:56 +0530

    Time: 61 ms (13.66%), Space: 30.9 MB (15.72%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> map;
+
+        for(string s:strs){
+            int count[26]={0};
+
+            for(char c: s){
+                count[c-'a']++;
+            }
+
+            string key="";
+
+            for(int i=0;i<26;i++){
+                key+="#";
+                key+=to_string(count[i]);
+            }
+            map[key].push_back(s);
+
+        }
+
+        vector<vector<string>> result;
+
+        for(auto & pair:map){
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
