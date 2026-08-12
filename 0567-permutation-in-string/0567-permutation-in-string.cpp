7f1cc048869fe8d4c3b8dff2b0ad25f65b1c41d3
Pravin Bhattarai <pravinbhattarai45@gmail.com>
2026-08-12 08:56:19 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.06%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
-bool matches(vector<int>m1,vector<int> m2){
-    for(int i=0;i<26;i++){
-        if(m1[i]!=m2[i]){
-            return false;
-        }
-       
-    }
-    return true;
-}
     bool checkInclusion(string s1, string s2) {
+      if(s1.length()>s2.length()){
+        return false;
+      }
+
+        int left=0;
+        vector<int>count1(26,0);
+       vector<int>count2(26,0);
+
+
+       
+
+       
+          for (int i = 0; i < s1.length(); i++) { // first window calculate gareko 
+            count2[s2[i] - 'a']++;
+            count1[s1[i]-'a']++;
+        }
+
+        if(count1==count2){
+            return true;    // s2 koh first window matai check gareko
+        }
+
+       for(int r=s1.length();r<s2.length();r++){  // s2 koh secod window dekhi start areko
+        count2[s2[r]-'a']++; // adding haracter
+
+       int l=r-s1.length();
+        count2[s2[l]-'a']--; // removing the characters
+
+        if(count1==count2){
+            return true;  
+        }
+
+
+       }
+
+    return false;
+
         
-        if (s1.length() > s2.length()) {
-            return false;
-        }
+    
 
-        vector<int> s1Map(26, 0);
-        vector<int> s2Map(26, 0);
 
-        for (int i = 0; i < s1.length(); i++) {
 
-            s1Map[s1[i] - 'a']++;
-
-            s2Map[s2[i] - 'a']++;
-        }
         
-
-        for(int i=s1.length();i<s2.length();i++){
-           if( matches(s1Map,s2Map)){
-           return true;
-           }
-
-            int l=i-s1.length();
-
-            s2Map[s2[i] - 'a']++;
-
-            s2Map[s2[l] - 'a']--;
-        }
-
-        return matches(s1Map,s2Map); // takes mre time
     }
 };
