fe56bcea6b86c24bedb8146a140db3281dae7c98
Pravin Bhattarai <pravinbhattarai45@gmail.com>
2026-08-14 07:08:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (98.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,23 +3,26 @@ public:
     bool isValid(string s) {
         stack<char> st;
 
-        for(char ch:s){
-            if(ch=='('||ch=='{'||ch=='['){
-                st.push(ch);
-            }else{
+        for(char c:s){
+            if(c=='('||c=='{'||c=='['){
+                st.push(c);
+            } else{
                 if(st.empty()){
                     return false;
                 }
             
-            char top=st.top();
-            if((ch=='}'&&top=='{')||(ch==')'&&top=='(')||(ch==']'&&top=='[')){
-                st.pop();
-            }else{
+
+
+                int top=st.top();
+
+                 if(c=='}'&&top=='{'||c==']'&&top=='['||c==')'&&top=='('){
+                 st.pop();
+                }else{
                 return false;
+                }
             }
         }
-        }
 
-return st.empty();
+        return st.empty();
     }
 };
