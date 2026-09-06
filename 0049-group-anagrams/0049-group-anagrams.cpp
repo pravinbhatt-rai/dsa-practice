c39a9ec53c569eb3c122df6426da29844ed3812c
Pravin Bhattarai <pravinbhattarai45@gmail.com>
2026-09-06 17:44:28 +0530

    Time: 11 ms (90.92%), Space: 25 MB (82.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> map;
+        unordered_map<string,vector<string>>map;
 
         for(string s:strs){
-            int count[26]={0};
-
-          string key=s; 
-          sort(key.begin(),key.end()) ;// abs and asb after soring asb become abc
-            map[key].push_back(s); // jun key cha teseima s lai pushback garde kina bhane key same huncha
-
+         string key=s;
+         sort(key.begin(),key.end());
+         map[key].push_back(s);
         }
-
-        vector<vector<string>> result;
+        vector<vector<string>>result;
 
         for(auto & pair:map){
             result.push_back(pair.second);
         }
-
         return result;
+        
     }
 };
